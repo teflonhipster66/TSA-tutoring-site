16cf1b1bab73c94d715969db79cb29e7d74535f4
teflonhipster66 <90221439+teflonhipster66@users.noreply.github.com>
2022-02-26 15:08:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+
+
+
+function submit(){
+    var Data = []
+    for (var i = 0; i < Data.length; i++)
+        Data[i] = []
+    var type = document.getElementById("roles").value;
+    var password= document.getElementById("psw").value;
+    var password2= document.getElementById("pswrepeat").value;
+    var username = document.getElementById("Username").value;
+    var email  = document.getElementById("email").value;
+    localStorage.setItem("StoredEmail", email)
+    
+
+    if(password!=password2){
+    alert("Passwords do not match")
+    }
+
+    if (type == "teacher") {
+        localStorage.setItem("teacherUser", username)
+        localStorage.setItem("teacherPassword", password)
+        localStorage.setItem("TypeCounter",  1)
+        Data[0] = localStorage.getItem("TypeCounter")
+        Data[1] = localStorage.getItem("StoredEmail")
+        Data[2] = localStorage.getItem("teacherUser")
+        Data[3] = localStorage.getItem("teacherPassword")
+    
+        localStorage.setItem("Array", JSON.stringify(Data))
+        
+    }
+    
+    if (type == "student") {
+        localStorage.setItem("StudentUser", username)
+        localStorage.setItem("studentPassword", password)
+        type = type + 2
+        localStorage.setItem("TypeCounter", 2)
+        Data[0] = localStorage.getItem("TypeCounter")
+        Data[1] = localStorage.getItem("StoredEmail")
+        Data[2] = localStorage.getItem("StudentUser")
+        Data[3] = localStorage.getItem("studentPassword")
+        localStorage.setItem("Array", JSON.stringify(Data))
+
+    }
+
+    if (password == password2){
+           window.open("login.html")
+        
+    }
+}
+
+function Incorrect() {
+    alert("Inncorect Username or Password")
+}
+
+function verify() {
+    console.time()
+
+
+    var Data = JSON.parse(localStorage.getItem("Array"))
+    console.log(Data)
+    var userInput = document.getElementById("enterUser").value
+    var pwInput = document.getElementById("enterPw").value;
+    var x = 0
+    var role = Data.indexOf(userInput) - 2
+    if (Data.includes(userInput)) {
+        var location = Data.indexOf(userInput)
+    }
+  
+    if (Data.includes(pwInput)) {
+        var pwLocation = Data.indexOf(pwInput)
+    }
+
+
+    if (pwLocation - location == 1 && Data[role] == 1) {
+        alert("Teacher Login Succesful")
+    }
+    else {
+        x = x + 1
+    }
+
+    if (pwLocation - location == 1 && Data[role] == 2) {
+        alert("Student Login succesful")
+    }
+    else {
+        x = x + 1
+    }
+    if (x == 2) {
+        Incorrect()
+    }
+
+
+    console.timeEnd()
+}
+
+
+function email() {
+    var type = localStorage.getItem("TypeCounter")
+    var GetEmail = localStorage.getItem("StoredEmail")
+    if (type == 1) { 
+        Email.send({
+            Host: "smtp.gmail.com",
+            Username: "TutoringAppTSA@gmail.com",
+            Password: "tutoringpassword",
+            To: GetEmail,
+            From: "TutoringAppTSA@gmail.com",
+            Subject: "Username and password Recovery",
+            Body: "Username: " + localStorage.getItem("teacherUser") + "Password: " + localStorage.getItem("teacherPassword"),
+    })
+            .then(function (message) {
+            alert("mail sent successfully")
+        });
+    
+    }
+
+    if (type == 2) {
+        Email.send ({
+            Host: "smtp.gmail.com",
+            Username: "TutoringAppTSA@gmail.com",
+            Password: "tutoringpassword",
+            To: GetEmail,
+            From: "TutoringAppTSA@gmail.com",
+            Subject: "Username and password Recovery",
+            Body: "Username: " + localStorage.getItem("StudentUser") + "Password: " + localStorage.getItem("studentPassword") ,
+        })
+            .then(function (message) {
+                alert("mail sent successfully")
+            
+        })
+    }
+
+}
